a5912757a33736c12d878b7444f1b44bade763e7
g-crow <genevieve.crow@gmail.com>
2019-12-01 09:16:57 -0700

    Create CRUD functions. Add route authentication and redirect logic.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_bkilKUbMjA4atuJiJJVhO0NB00QOQILxtX",
   s3: {
     REGION: "us-east-2",
     BUCKET: "my-awesome-example-app-123"
