877742405c0a3baaefb30ea017e9d8ee5865425f
g-crow <genevieve.crow@gmail.com>
2019-12-05 20:42:05 -0700

    Config for deployment. Add Netlify build script.

--- src/config.js
+++ src/config.js
@@ -1,18 +1,46 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   STRIPE_KEY: "pk_test_bkilKUbMjA4atuJiJJVhO0NB00QOQILxtX",
   s3: {
     REGION: "us-east-2",
-    BUCKET: "my-awesome-example-app-123"
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-ppdpr6z8sbc4"
   },
   apiGateway: {
     REGION: "us-east-2",
-    URL: "https://xksd3qyvna.execute-api.us-east-2.amazonaws.com/prod"
+    URL: "https://vmlh3od7m5.execute-api.us-east-2.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-2",
-    USER_POOL_ID: "us-east-2_Ya8EaB2Zr",
-    APP_CLIENT_ID: "6i2lhm5mc880k899e7hfcbm3bc",
-    IDENTITY_POOL_ID: "us-east-2:63d4b254-3dcb-47a8-9039-ed89fdece9ff"
+    USER_POOL_ID: "us-east-2_dRWW7oBaG",
+    APP_CLIENT_ID: "6c24sqeg58mnlqsrvlmabqfatp",
+    IDENTITY_POOL_ID: "us-east-2:7ea3d7c0-805c-4315-aefa-8c2e8fa82ecf"
   }
+};
+
+const prod = {
+  STRIPE_KEY: "pk_test_bkilKUbMjA4atuJiJJVhO0NB00QOQILxtX",
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-vlc2k8xuqvgd"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://4e3olesiih.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_ypJN0B4uW",
+    APP_CLIENT_ID: "7vt89o1m1dgd0fuuf8at5q9nfr",
+    IDENTITY_POOL_ID: "us-east-2:c1ba1b24-195f-4ae8-9438-ff4eef951670"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
